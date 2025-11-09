83a852c61cc0515158a24bb1a3ced15ce03390b3
Piotr Tadeusz Józef Jankiewicz <piotrjankiewicz02@gmail.com>
2025-11-09 20:53:14 +0100

    chapter 3: variables in progress

---
+++ 03_variables/src/main.rs
@@ -0,0 +1,40 @@
+
+
+
+fn main() {
+    // let x = 5;
+    // println!("The value of x is: {}", x);
+    // // first x is shadowed by the second x!!
+    // let x = "six";
+    // println!("The value of x is: {}", x);
+
+    // const SUBSCRIBER_COUNT: u32 = 100000;
+
+    // scalar types vs compound types
+
+
+    // intigers 
+    let a = 98_222;
+    let b: u8 = 255;
+    let c: i8 = -128;
+    // binaruy 
+    let d: u8 = 0b1111_0000;
+    // octal
+    let e: u8 = 0o77;
+    // hexadecimal
+    let f: u8 = 0xFF;
+    // byte
+
+    let f: isize = 64;
+    // floiating point numbers 
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+    // booleans 
+    let t = true;
+    let f: bool = false;
+    // characters
+    let c = 'z';
+    let z: char = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+}
