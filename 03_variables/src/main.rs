a242ecfd30594a5b55703cd81a7a583e3cb723d8
Piotr Tadeusz Józef Jankiewicz <piotrjankiewicz02@gmail.com>
2025-11-10 14:29:18 +0100

    chapter 3: loops, functions

--- 03_variables/src/main.rs
+++ 03_variables/src/main.rs
@@ -1,6 +1,3 @@
-
-
-
 fn main() {
     // let x = 5;
     // println!("The value of x is: {}", x);
@@ -12,12 +9,11 @@ fn main() {
 
     // scalar types vs compound types
 
-
-    // intigers 
+    // intigers
     let a = 98_222;
     let b: u8 = 255;
     let c: i8 = -128;
-    // binaruy 
+    // binaruy
     let d: u8 = 0b1111_0000;
     // octal
     let e: u8 = 0o77;
@@ -26,10 +22,10 @@ fn main() {
     // byte
 
     let f: isize = 64;
-    // floiating point numbers 
+    // floiating point numbers
     let x = 2.0; // f64
     let y: f32 = 3.0; // f32
-    // booleans 
+                      // booleans
     let t = true;
     let f: bool = false;
     // characters
@@ -38,6 +34,44 @@ fn main() {
     let heart_eyed_cat = '😻';
 
     // compound types
-    
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (channel, sub_count, active) = tup;
+    let sub_count2 = tup.1;
 
+    // loops can return values
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    // different kinds of loops
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    // iterating through a collection with for
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+    // functions, there is no return keyword, just expressions
+    fn five() -> i32 {
+        5
+    }
+
+    let x = five();
+    println!("The value of x is: {}", x);
 }
